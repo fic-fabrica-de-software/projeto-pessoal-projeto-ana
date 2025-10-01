c9348a679d5500d09a4c0697f5e0cf6eababd650
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-10-01 07:45:40 -0300

    Add - style

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+        <div>
+            <img class="imagemlogo" src="" alt="Logo do app">
+        </div>
+
+    </main>
+    
+</body>
+</html>
