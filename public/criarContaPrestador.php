75f65ca260dd76af326d28d8b8f00f155c02da70
Ana <ana.l.amaral@estudante.sesisenai.org.br>
2025-10-03 09:16:48 -0300

    Add - hr

--- public/criarContaPrestador.php
+++ public/criarContaPrestador.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Cadastro Prestador</title>
     <link rel="stylesheet" href="../styles/style.css">
 </head>
 
@@ -98,6 +98,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="infocadastro">
                         <input type="password" name="senha" placeholder="Senha" required>
                     </div>
+
+                    <hr>
 
                     <input class="botaoinverso" type="submit" value="Continue">
                 </form>
