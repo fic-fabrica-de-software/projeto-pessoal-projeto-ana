5329a6d84d0b95a4768861ca0be7698dc07e7ac1
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-10-07 07:45:54 -0300

    Edit -- style página login

--- public/termosDeUso.php
+++ public/termosDeUso.php
@@ -13,28 +13,27 @@
 <body>
     <div class="termodeuso">
 
-        <p class="termos">
-            Concordamos em fornecer a você o Serviço do Prestaris. O Serviço inclui todos os produtos, recursos, apps, serviços, tecnologias e software do Prestaris
-            que fornecemos para promover a missão do Instagram: fortalecer seus relacionamentos com as pessoas e com as coisas que você adora. 
+        <p>
+            Concordamos em fornecer a você o Serviço do Prestaris. O serviço inclui todos os produtos, recursos, apps, serviços, tecnologias e software do Prestaris
+            que fornecemos para promover a missão de fortalecer seus relacionamentos com as pessoas e cuidar do que você ama. 
             O Serviço é composto pelos seguintes aspectos:
         </p>
-        <p class="topicos">
+        <p>
             <p><strong>•</strong> Oferecer oportunidades personalizadas de criar, conectar, comunicar, descobrir e compartilhar.</p>
             <p><strong>•</strong> Promover um ambiente seguro, inclusivo e positivo.</p>
             <p><strong>•</strong> Desenvolver e usar tecnologias que nos ajudam a atender com consistência nossa crescente comunidade.</p>
             <p><strong>•</strong> Garantir acesso ao nosso Serviço.</p>
             <p><strong>•</strong> Conectar você com marcas, produtos e serviços de maneiras importantes para você.</p>
-            <p><strong>•</strong> Pesquisar e inovar.
         </p>
-        <p class="termos">
-            Prestar nosso Serviço exige a coleta e o uso das suas informações. A Política de Privacidade explica como coletamos, 
-            usamos e compartilhamos informações. Ela também explica as várias formas como você pode controlar suas informações, 
-            inclusive nas Configurações de Privacidade e Segurança do Prestaris. Você deve concordar com a Política de Privacidade para usar o Prestaris.
+        <p>
+            Prestar nosso serviço exige a coleta e o uso das suas informações. A Política de Privacidade explica como coletamos, 
+            usamos e compartilhamos informações. Ela também explica as várias formas como você pode controlar suas informações. 
+            Você deve concordar com a Política de Privacidade para usar o Prestaris.
         </p>
 
     </div>
 
-    <button class="botao"> Continue</button>
+    <a href="login.php"><button class="botao"> Continue</button></a>
 
 </body>
 </html>
